055162044288e0b6b8fd8cdf1429a9be8eb9ece4
Timo Derstappen <teemow@gmail.com>
2015-01-16 22:46:26 +0100

    add sigint/sigterm handling

--- ping.go
+++ ping.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-    "github.com/hoisie/web"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hoisie/web"
 )
 
 func hello(val string) string {
-    return "hello"
+	return "hello"
 }
 
 func main() {
-    web.Get("/(.*)", hello)
-    web.Run("0.0.0.0:80")
+	web.Get("/(.*)", hello)
+	go web.Run("0.0.0.0:80")
+
+	// Handle SIGINT and SIGTERM.
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	// Stop the service gracefully.
+	web.Close()
 }
